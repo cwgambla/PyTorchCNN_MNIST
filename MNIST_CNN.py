ab2dcb01dbd26293fe65dd55b68531b62155f45a
cgambla2020@gmail.com <cgambla2020@gmail.com>
2023-09-04 14:14:43 -0500

    Second Commit

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -3,11 +3,12 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+import torch.nn.functional as F
 from torchvision import transforms
 import torch.optim as optim
 import numpy as np
 
-torch.manual_seed(42)
+torch.manual_seed(39)
 
 
 # train_x = torch.tensor(np.array([np.random.rand(1,28*28),np.random.rand(1,28*28)]))
@@ -24,40 +25,48 @@ Lambda = (lambda y: torch.zeros(10, dtype=torch.float).scatter_(0,torch.tensor(y
 
 train = MNIST(root=".",train=True,transform=transform,download=True, target_transform=Lambda)
 test = MNIST(root=".",train=False,transform=transform,download=True, target_transform=Lambda)
-# train = torch.tensor(train, dtype=torch.float)
-# test = torch.tensor(test, dtype=torch.float)
-class myModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.layers = nn.Sequential(
-            nn.Linear(28*28,512),
-            nn.ReLU(),
-            nn.Linear(512,256),
-            nn.ReLU(),
-            nn.Linear(256,128),
-            nn.ReLU(),
-            nn.Linear(128,10),
 
-            #only comment out this layer if CCELoss is used
-            nn.Sigmoid() 
-        )
+class CNN(nn.Module):
+
+    def __init__(self, in_channels = 1, num_class = 10):
+        super(CNN,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=32,kernel_size=(5,5),padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32,out_channels=32,kernel_size=(5,5),padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32,out_channels=64,kernel_size=(5,5),padding=1)
+        self.conv4= nn.Conv2d(in_channels=64,out_channels=64,kernel_size=(5,5),padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=(2,2),stride = (2,2))
+        self.fc1 = nn.Linear(1024,120)
+        self.fc2 = nn.Linear(120,40)
+        self.fc3 = nn.Linear(40,num_class)
+        
+
+        self.drop = nn.Dropout(.25)
+
+    def forward(self,x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+        
+        x = x.reshape(x.shape[0],-1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
 
 
-    def forward(self, x):
-
-        #flattens input
-        x = self.flatten(x)
-
-        #runs the input throught the neural net
-        #and then returns the expected output
-        logits = self.layers(x)
-        return logits
+        return x
     
 
-model = myModel()
+model = CNN()
 
-loss_fn = nn.BCELoss()#Means Squared Error Function
+x = torch.randn(64,1,28,28)
+print(model(x).shape)
+
+model = CNN()
+
+loss_fn = nn.CrossEntropyLoss()#Means Squared Error Function
 
 optimizer = optim.Adam(model.parameters(), lr=0.001)#optimizer used to update weights, using Scholastic gradient decent
 
@@ -70,7 +79,7 @@ test_loader = DataLoader(test,batch_size=batch_size)
 
 
 #trainginng mooodel
-epochs = 1
+epochs = 10
 for epoch in range(epochs):
 
     #predicting x
@@ -84,7 +93,7 @@ for epoch in range(epochs):
         optimizer.step()
 
         if (index + 1) % 100 == 0:
-            print(f'Epoch [{epoch+1}/{epochs}], Step [{index+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+            print(f'Epoch [{epoch+1}/{epochs}], Step [{index+1}/{len(train_loader)}], Loss: {loss.item():.6f}')
 
 
 #testing accuracy
